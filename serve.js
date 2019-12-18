0efd1364faf07b969fc91aac8fcd17990fb68495
R. Hannibal B. Moulvad <hmoulvad@hotmail.com>
2019-12-18 10:59:29 +0100

    Maybe this will serve the stylesheet on the server.

--- serve.js
+++ serve.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(favicon(__dirname + "/public/favicon.ico"));
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + "/public"));
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get("/ping", function(req, res) {
