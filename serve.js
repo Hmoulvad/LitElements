c8d89c1cc8c58cb80a7f64c3f111d44150ae7522
R. Hannibal B. Moulvad <hmoulvad@hotmail.com>
2019-12-18 10:21:17 +0100

    We try this now.

--- serve.js
+++ serve.js
@@ -1,12 +1,16 @@
 const express = require('express');
-const { join } = require('path');
+const path = require("path");
 const favicon = require("express-favicon");
-
+const port = process.env.PORT || 3000;
 const app = express();
-const port = 3000;
+
 app.use(favicon(__dirname + "/public/favicon.ico"));
-app.use(express.static(join(__dirname, 'public')));
-app.use(express.static('./dist'));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get("/ping", function(req, res) {
+    return res.send("pong");
+});
 
 app.get("/*", function(req, res) {
     res.sendFile(path.join(__dirname, "public", "index.html"));
