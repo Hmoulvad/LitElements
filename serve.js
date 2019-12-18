bae755f4abec8648118eda7db2b3678170cb5f8f
R. Hannibal B. Moulvad <hmoulvad@hotmail.com>
2019-12-18 10:15:53 +0100

    Maybe this builds onto Heroku.

--- serve.js
+++ serve.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const { join } = require('path');
+const favicon = require("express-favicon");
 
 const app = express();
 const port = 3000;
-
+app.use(favicon(__dirname + "/public/favicon.ico"));
 app.use(express.static(join(__dirname, 'public')));
 app.use(express.static('./dist'));
+
+app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.listen(port, () => console.log(`App running on ${port}.`));
--- src/shell/footer/footer.ts
+++ src/shell/footer/footer.ts
@@ -1,18 +1,14 @@
-import { LitElement, html, customElement, property } from "lit-element";
-import { resolve } from "path";
+import { LitElement, html, customElement } from "lit-element";
 import { global } from "../../styles/global.css";
 
 @customElement("app-footer")
 class Footer extends LitElement {
-
-    @property() test = resolve(__dirname, "./footer.css");
 
     static get styles() {
         return global
     }
 
     render() {
-        console.log("Test", this.test);
         return html`
             <footer>
                 <p>This is the Footer of the Web Application</p>
