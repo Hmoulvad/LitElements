7641af434603d29ece112f179b8aa09dd693308c
R. Hannibal B. Moulvad <hmoulvad@hotmail.com>
2019-12-18 12:38:19 +0100

    Pointing to dist now.

--- serve.js
+++ serve.js
@@ -4,8 +4,8 @@ const favicon = require("express-favicon");
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(favicon(__dirname + "/public/favicon.ico"));
-app.use(express.static(__dirname + "/public"));
+app.use(favicon(__dirname + "/dist/favicon.ico"));
+app.use(express.static(__dirname + "/dist"));
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get("/ping", function(req, res) {
@@ -13,7 +13,7 @@ app.get("/ping", function(req, res) {
 });
 
 app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 app.listen(port, () => console.log(`Application running on port: ${port}.`));
